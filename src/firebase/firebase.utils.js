fa870ddd71aae257c0b5b7e8275065e468442e7c
Pratik Dandhare <iampratik@criptext.com>
2021-02-09 14:23:16 +0530

    implement code that pulls in our shop data from firestore and stores it into the shop reducer from our shop page also included our withSpinner HOC

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,18 +1,20 @@
-import firebase from "firebase/app";
-import "firebase/firestore";
-import "firebase/auth";
-
-require('dotenv').config();
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
 
 const config = {
-  apiKey: "AIzaSyDe-GlJXmZMtevuFT4nZbyf555ctXdPWEw",
-  authDomain: "crwn-db-9366b.firebaseapp.com",
-  projectId: "crwn-db-9366b",
-  storageBucket: "crwn-db-9366b.appspot.com",
-  messagingSenderId: "244403369394",
-  appId: "1:244403369394:web:4e838e27d1f0c9ec6fe0ab",
-  measurementId: "G-GTBPZLNETG",
+  apiKey: 'AIzaSyDe-GlJXmZMtevuFT4nZbyf555ctXdPWEw',
+  authDomain: 'crwn-db-9366b.firebaseapp.com',
+  databaseURL: "https://crwn-db-9366b-default-rtdb.firebaseio.com",
+  projectId: 'crwn-db-9366b',
+  storageBucket: 'crwn-db-9366b.appspot.com',
+  messagingSenderId: '244403369394',
+  appId: '1:244403369394:web:4e838e27d1f0c9ec6fe0ab',
+  measurementId: 'G-GTBPZLNETG',
+
 };
+
+firebase.initializeApp(config);
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return;
@@ -24,7 +26,6 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!snapShot.exists) {
     const { displayName, email } = userAuth;
     const createdAt = new Date();
-
     try {
       await userRef.set({
         displayName,
@@ -33,20 +34,51 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
         ...additionalData,
       });
     } catch (error) {
-      console.log("error creating user", error.message);
+      console.log('error creating user', error.message);
     }
   }
 
   return userRef;
 };
 
-firebase.initializeApp(config);
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = firestore.collection(collectionKey);
+
+  const batch = firestore.batch();
+  objectsToAdd.forEach((obj) => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
+  });
+
+  return await batch.commit();
+};
+
+export const convertCollectionsSnapshotToMap = (collections) => {
+  const transformedCollection = collections.docs.map((doc) => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items,
+    };
+  });
+
+  return transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {});
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: "select_account" });
+provider.setCustomParameters({ prompt: 'select_account' });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
